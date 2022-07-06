3e29137f0950b045eb589d0e2baa0cbf72a5acd4
Datka <32612854+zhusupmamatzhanov@users.noreply.github.com>
2022-07-06 21:31:39 +0600

    I create new class

--- com/company/Main.java
+++ com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nurmuhammad");
-        System.out.println("Datka");
+
+        Student student  = new Student("Datka",21);
+        Student student2 = new Student("Nurmuhammad",19);
+        List<Student>list = new ArrayList<>();
+        list.add(student);
+        list.add(student2);
+        System.out.println(list);
+
+
+
     }
 }
